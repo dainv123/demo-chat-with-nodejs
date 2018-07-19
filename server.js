bf2c1e983240d507b4d454cf2057f20cfffed175
dai nguyen <ngdai0402@gmail.com>
2018-07-19 23:35:07 +0700

    init

--- server.js
+++ server.js
@@ -1,63 +1,57 @@
-// const http = require("http");
-// const fs = require("fs");
-// var proxy = http.createServer((req, res) => {
-//   var rfile = fs.createReadStream(__dirname + "/demo.html");
-//   var obj = { id: 1, name: "dai" };
-//   if (req.url === "/") {
-//     rfile.pipe(res);
-//   } else {
-//     res.writeHead(200, { "Content-Type": "application/json" });
-//     res.end(JSON.stringify(obj));
-//   }
-// });
-
-// proxy.listen(1129, "127.0.0.1");
-
-require("http").createServer(function(req, res) {
-  var fs = require("fs"),
-    tmpl = require("blueimp-tmpl"),
-    data = {
-      title: "JavaScript Templates",
-      url: "https://github.com/blueimp/JavaScript-Templates",
-      features: ["lightweight & fast", "powerful", "zero dependencies"]
-    };
-  tmpl.load = function(id) {
-    var filename = id + ".html";
-    console.log("Loading " + filename);
-    return fs.readFileSync(filename, "utf8");
-  };
-  res.writeHead(200, { "Content-Type": "text/html" });
-  res.end(tmpl("template", data));
-});
-
-var express = require("express");
-var app = express();
 var fs = require("fs"),
-  tmpl = require("blueimp-tmpl"),
-  data = {
-    title: "JavaScript Templates",
-    url: "https://github.com/blueimp/JavaScript-Templates",
-    features: ["lightweight & fast", "powerful", "zero dependencies"]
-  };
-
-app.get("/", function(req, res) {
-  res.send("Hello World");
-});
-
-app.get("/contact/:id/send/:content", function(req, res) {
-  res.send("Result__ id:" + req.params.id + " __content:" + req.params.content);
-});
-
-app.get("/orther", function(req, res) {
-  tmpl.load = function(id) {
-    var filename = id + ".html";
-    console.log("Loading " + filename);
-    return fs.readFileSync(filename, "utf8");
-  };
-  res.writeHead(200, { "Content-Type": "text/html" });
-  res.end(tmpl("template", data));
-});
+    tmpl = require("blueimp-tmpl"),
+    express = require("express");
+var app = express();
 
 app.use("/assets", express.static("public"));
 
-app.listen(3000);
+app.get("/", function (req, res) {
+    res.send("Hello World");
+});
+
+app.get("/contact/:id/send/:content", function (req, res) {
+    res.send("Result__ id:" + req.params.id + " __content:" + req.params.content);
+});
+
+app.get("/:content", function (req, res) {
+    var data = {
+        title: "JavaScript Templates",
+        url: "https://github.com/",
+        features: ["lightweight & fast", "powerful", "zero dependencies"]
+    };
+
+    tmpl.load = function (id) {
+        var filename = './page/' + id + ".html";
+        return fs.readFileSync(filename, "utf8");
+    };
+
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end(tmpl(req.params.content, data));
+});
+
+var server = require('http').Server(app)
+var io = require('socket.io')(server);
+
+io.on('connection', function (socket) {
+    console.log("Chao mung: " + socket.id);
+    socket.emit('sendsv', { hello: 'server' });
+    socket.on('disconnect', function () {
+        console.log(socket.id + ' ________ is Leave, :((');
+    });
+
+    socket.on('Send data to server', function (data) {
+        console.log('Mess from ' + socket.id + ': ' + data.sms);
+        var post_mess = { "userid": socket.id, "sms": data.sms };
+        // socket.emit('Send data to client', post_mess);
+        // socket.broadcast.emit('Send data to client', post_mess);
+        // io.to.emit(____socket.id____);
+        io.sockets.emit('Send data to client', post_mess);
+    });
+
+    socket.on('Send background to server', function (data) {
+        var post_mess = { "userid": socket.id, "background": data.sms };
+        io.sockets.emit('Send background to client', post_mess);
+    });
+});
+
+server.listen(3000);
